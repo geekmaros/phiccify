0a396f416ce0b42748d4297400acb1e0920a5a78
geekmaros <razicruz@gmail.com>
2020-02-08 17:45:47 +0100

    broken homepage to diff component

--- src/App.js
+++ src/App.js
@@ -1,10 +1,11 @@
 import React from 'react';
-import './App.css';
-import HomePage from "./Homepage.component";
+import './App.sass';
+import HomePage from "./pages/homepage/homepage.component";
+
 
 function App() {
   return (
-    <div className="App">
+    <div>
       <HomePage/>
     </div>
   );
---
+++ src/pages/homepage/homepage.component.jsx
@@ -0,0 +1,8 @@
+import React from 'react'
+import Directory from '../../components/directory/directory.components'
+import './homepage.styles.sass'
+
+const HomePage = () => (<div className="homepage"> <Directory/></div>)
+
+
+export default HomePage;
