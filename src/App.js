f704c439d0bf164a4253926f083112b0f2c84814
geekmaros <razicruz@gmail.com>
2020-02-10 12:56:30 +0100

    added routing to  navigation our App using react-router-dom

--- src/App.js
+++ src/App.js
@@ -2,13 +2,16 @@ import React from 'react';
 import {Switch, Route } from 'react-router-dom'
 import './App.sass';
 import HomePage from "./pages/homepage/homepage.component";
+import Hats from './pages/hats/hats.component'
 
 
 function App() {
   return (
     <div>
       <Switch>
-        <Route exact path="/" component={HomePage}/>
+        <Route exact  path="/" component={HomePage}/>
+        <Route   path="/shop/hats" component={Hats}/>
+        <Route   path="/hats/:id" component={Hats}/>
       </Switch>
     </div>
   );
---
+++ src/pages/hats/hats.component.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+import './hats.styles.sass'
+
+const Hats = (props) => {
+    console.log(props)
+ return (
+     <div><h1>Hats Page</h1></div>
+ )
+}
+
+export default Hats
