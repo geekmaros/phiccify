94db4b8b7e83326a4392ac127aec67808be45b61
geekmaros <razicruz@gmail.com>
2020-02-13 17:59:44 +0100

    added firebase to app and signIn with Google

--- src/components/sign-in/sign-in.component.jsx
+++ src/components/sign-in/sign-in.component.jsx
@@ -2,6 +2,7 @@ import React, {Component} from "react";
 import './sign-in.styles.sass'
 import FormInput from "../form-input/form-input.component";
 import CustomButton from "../custom-button/custom-button.component";
+import {signInWithGoogle} from "../../firebase/firebase.utils";
 
 class SignIn extends Component{
     constructor(props) {
@@ -31,7 +32,12 @@ class SignIn extends Component{
                     <FormInput name='email' type='email' value={this.state.email} label='email' handleChange={this.handleChange} required />
                     <FormInput name='password' type='password' value={this.state.password} label='password' handleChange={this.handleChange} required />
 
-                    <CustomButton type='submit'>Sign In</CustomButton>
+                    <div className='sign-in-container'>
+                        <CustomButton type='submit'>Sign In</CustomButton>
+                        <CustomButton  onClick={signInWithGoogle}>
+                            Sign In With Google
+                        </CustomButton>
+                    </div>
                 </form>
             </div>
         );
---
+++ src/firebase/firebase.utils.js
@@ -0,0 +1,25 @@
+import firebase from "firebase/app";
+import 'firebase/firestore'
+import 'firebase/auth'
+
+const config = {
+    apiKey: "AIzaSyAdGbu8p1k6t8YYnMa-vnXPQCmVwGDnfkY",
+    authDomain: "phiccify-db.firebaseapp.com",
+    databaseURL: "https://phiccify-db.firebaseio.com",
+    projectId: "phiccify-db",
+    storageBucket: "phiccify-db.appspot.com",
+    messagingSenderId: "902113502496",
+    appId: "1:902113502496:web:aa7ade48e7b444e309a345",
+    measurementId: "G-SRGHLJZ3G4"
+};
+
+firebase.initializeApp(config);
+export const auth = firebase.auth();
+export const firestore = firebase.firestore();
+
+const Provider = new firebase.auth.GoogleAuthProvider();
+Provider.setCustomParameters({prompt: 'select_account'});
+
+export const signInWithGoogle = () => auth.signInWithPopup(Provider);
+
+export default firebase
