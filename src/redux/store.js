9d823762eb66658a807aa77f5544587364cbc967
geekmaros <razicruz@gmail.com>
2020-04-06 16:59:13 +0100

    added redux devtool extension

--- src/redux/store.js
+++ src/redux/store.js
@@ -1,9 +1,9 @@
-import { createStore, applyMiddleware} from "redux";
+import { createStore, applyMiddleware, compose} from "redux";
 import rootReducer from './root-reducer'
 import logger from 'redux-logger'
 
 const middlewares = [logger]
-
-const store = createStore(rootReducer, applyMiddleware(...middlewares))
+const composeEnhancer =  window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose()
+const store = createStore(rootReducer, composeEnhancer(applyMiddleware(...middlewares)))
 
 export default store
