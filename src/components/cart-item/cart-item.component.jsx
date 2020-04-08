cfe3bd40360bdc59c9732eff5b9fe4e49b2fe031
geekmaros <razicruz@gmail.com>
2020-04-08 15:09:17 +0100

    customized cart item

---
+++ src/components/cart-item/cart-item.component.jsx
@@ -0,0 +1,24 @@
+import React from "react";
+
+
+import './cart-item.styles.sass'
+
+
+const CartItem = ({item: {imageUrl, price, name, quantity}}) => (
+    <div className='cart-item'>
+        <img src={imageUrl} alt={name}/>
+
+        <div className="item-details">
+            <span className='name'>{name}</span>
+            <span className='price'>
+                {quantity} x ${price}</span>
+
+        </div>
+
+       <div>
+           <button className='remove-item'>X</button>
+       </div>
+    </div>
+)
+
+export default CartItem
