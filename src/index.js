c56b5d6640892e1a1e1a51bcb65e731d562ca468
geekmaros <razicruz@gmail.com>
2020-04-16 19:30:18 +0100

    added redux-persist to keep state data on reload

--- src/index.js
+++ src/index.js
@@ -2,6 +2,7 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import { BrowserRouter} from 'react-router-dom'
 import { Provider } from 'react-redux'
+import {PersistGate} from "redux-persist/integration/react";
 import './index.css';
 import App from './App';
 import 'pace-js'
@@ -9,13 +10,15 @@ import 'pace-js/themes/red/pace-theme-minimal.css'
 
 
 import * as serviceWorker from './serviceWorker';
-import store from './redux/store'
+import {store, persistor} from './redux/store'
 
 ReactDOM.render(
 //    the provider component is needed to warp around our app to enable us use redux
 <Provider store={store}>
 <BrowserRouter>
+<PersistGate persistor={persistor}>
 <App />
+</PersistGate >
 </BrowserRouter>
 </Provider>
 , document.getElementById('root'));
--- src/redux/store.js
+++ src/redux/store.js
@@ -1,9 +1,11 @@
 import { createStore, applyMiddleware, compose} from "redux";
+import {persistStore} from "redux-persist";// this helps us persist or keep data even on reload
 import rootReducer from './root-reducer'
 import logger from 'redux-logger'
 
 const middlewares = [logger]
 const composeEnhancer =  window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose()
-const store = createStore(rootReducer, composeEnhancer(applyMiddleware(...middlewares)))
+export const store = createStore(rootReducer, composeEnhancer(applyMiddleware(...middlewares)))
+export const persistor = persistStore(store)
 
-export default store
+export default {store, persistor  }
